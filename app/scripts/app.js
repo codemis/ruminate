52583b42a52a2c8e972eeebdfe0be6e6662702fb
Codemis <johnathan@jpulos.com>
2015-10-03 15:44:55 -0700

    Added a createAndLogin for the new users.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -6,7 +6,7 @@
 // the 2nd parameter is an array of 'requires'
 // 'starter.services' is found in services.js
 // 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers', 'parse.services', 'config'])
+angular.module('starter', ['ionic', 'starter.controllers', 'parse.services', 'ionic.utilities', 'config'])
 
 .run(['$ionicPlatform', 'ParseService', 'ENV', function($ionicPlatform, ParseService, ENV) {
   $ionicPlatform.ready(function() {
--- app/scripts/parse.services.js
+++ app/scripts/parse.services.js
@@ -1,5 +1,5 @@
 'use strict';
-
+/*global Parse */
 angular.module('parse.services', [])
 
 /**
@@ -7,7 +7,7 @@ angular.module('parse.services', [])
  *
  * @author Johnathan Pulos <johnathan@missionaldigerati.org>
  */
-.factory('ParseService', function() {
+.factory('ParseService', ['ParseUser', function(ParseUser) {
   /**
    * The object for the parse service
    *
@@ -21,23 +21,23 @@ angular.module('parse.services', [])
    * @param  {String} appId         The Parse Application Id
    * @param  {String} javascriptKey The Parse Javascript Key
    *
-   * @return {Void}
+   * @return {Promise} A promise to login the user
    *
    * @author Johnathan Pulos <johnathan@missionaldigerati.org>
    */
   parseServiceObject.initialize = function(appId, javascriptKey) {
-    console.log('INITIALIZE: ' + appId + ' - ' +javascriptKey);
     Parse.initialize(appId, javascriptKey);
+    return ParseUser.createAndLogin();
   };
 
   return parseServiceObject;
-})
+}])
 /**
  * A Service for handling ParseUser's model
  *
  * @author Johnathan Pulos <johnathan@missionaldigerati.org>
  */
-.factory('ParseUser', function() {
+.factory('ParseUser', ['$localStorage', function($localStorage) {
   /**
    * The parseUser Object that will be returned
    *
@@ -45,5 +45,75 @@ angular.module('parse.services', [])
    */
   var parseUserObject = {};
 
+  /**
+   * Create a GUID for the username
+   *
+   * @return {String} The username
+   *
+   * @author Johnathan Pulos <johnathan@missionaldigerati.org>
+   */
+  function createUsername() {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+      var r = Math.random()*16|0;
+      var v = (c === 'x') ? r : (r&0x3|0x8);
+      return v.toString(16);
+    });
+  }
+  /**
+   * Create a Random password to the set length
+   *
+   * @param  {Integer} length The length of the password
+   *
+   * @return {String}         The new password
+   *
+   * @author Johnathan Pulos <johnathan@missionaldigerati.org>
+   */
+  function createPassword(length) {
+    var charset = 'abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+    var password = '';
+    for (var i = 0, n = charset.length; i < length; ++i) {
+        password += charset.charAt(Math.floor(Math.random() * n));
+    }
+    return password;
+  }
+  /**
+   * Create the user if it does not exist, and log them in
+   *
+   * @return {Promise} A promise to login or sign up
+   *
+   * @author Johnathan Pulos <johnathan@missionaldigerati.org>
+   */
+  parseUserObject.createAndLogin = function() {
+    var userData = $localStorage.getObject('parse_user');
+    if (Object.keys(userData).length === 0) {
+      var user = new Parse.User();
+      var username = createUsername().toString();
+      var password = createPassword(12).toString();
+      /**
+       * create and login
+       */
+      user.set('username', username);
+      user.set('password', password);
+
+      return user.signUp(null, {
+        success: function() {
+          $localStorage.setObject('parse_user', {username: username, password: password});
+        },
+        error: function(user, error) {
+          console.log('Unable to sign up:  ' + error.code + ' ' + error.message);
+        }
+      });
+    } else {
+      console.log('I am a user.');
+      return Parse.User.logIn(userData.username, userData.password, {
+        success: function() {
+        },
+        error: function(user, error) {
+          console.log('Unable to login:  ' + error.code + ' ' + error.message);
+        }
+      });
+    }
+  };
+
   return parseUserObject;
-});
+}]);

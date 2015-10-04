7aabaac951f71ed6b2cfbd205d231036c5f17e75
phillip-zahir <phillip@somidata.com>
2015-10-03 21:44:37 -0700

    Fixed an issue with the display of the frequency setting in the settings tab.  Made it into a dropdown instead of a free-text field.

--- app/scripts/controllers.js
+++ app/scripts/controllers.js
@@ -228,4 +228,8 @@ angular.module('starter.controllers', [])
   };
 }])
 
-.controller('SettingsController', function($scope) {});
+.controller('SettingsController', function($scope) {
+
+$scope.minuteSelection = [{value:5,text:'5 minutes',selected:false}, {value:10,text:'10 minutes',selected:true}, {value:20,text:'20 minutes',selected:false}, {value:30,text:'30 minutes',selected:false}, {value:60,text:'1 hour',selected:false}, {value:120,text:'2 hours',selected:false}, {value:240,text:'4 hours',selected:false}];
+
+});

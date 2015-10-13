6e432e710b3a4a20744505a9a2ed1868545ac914
Codemis <johnathan@jpulos.com>
2015-10-13 15:40:11 -0700

    Added untested code for the push notification.

--- parse/cloud/ruminate-task.js
+++ parse/cloud/ruminate-task.js
@@ -4,27 +4,82 @@
  * @author Johnathan Pulos <johnathan@missionaldigerati.org>
  */
 Parse.Cloud.job('ruminateTask', function(request, status) {
-  Parse.Push.send({
-    channels: ['user-2d979933-b64a-4bbb-992a-efec4f34e3bc'],
-    data: {
-      alert: 'I am sending you message from the cloud :)'
-    }
-  },
-  {
-    success: function() {
-      status.success('Parse.Push.send completed successfully.');
-    },
-    error: function() {
-      status.error('Uh oh, something went wrong with Parse.Push.send.');
-    }
+  /**
+   * Get the upcoming tasks
+   */
+  var rightnow = new Date();
+  var query = new Parse.Query('RuminateTask');
+  var todaysDate = new Date(rightnow.getTime());
+  var midnight = new Date();
+  midnight.setHours(0);
+  midnight.setMinutes(0);
+  query.lessThanOrEqualTo('sendAt', todaysDate);
+  query.equalTo('completed', false);
+  query.each(function(task) {
+    /**
+     * Add a new question to reflect on.  And generate a message to send
+     * 1.  Check for all the complete questions today
+     */
+    var responseQuery = new Parse.Query('Response');
+    responseQuery.greaterThanOrEqualTo('createdAt', midnight);
+    responseQuery.lessThanOrEqualTo('createdAt', new Date());
+    responseQuery.equalTo('reflection', task.get('reflection'));
+    var keys = [];
+    responseQuery.each(function(response) {
+      /**
+       * 2. Create an array of keys for all questions already asked
+       */
+      keys.push(response.get('question').id);
+    }).then(function() {
+      /**
+       * 3. Find a question
+       */
+      var questionQuery = new Parse.Query('Question');
+      questionQuery.notContainedIn('objectId', keys);
+      questionQuery.limit(1);
+      questionQuery.find({
+        success: function(question) {
+          /**
+           * Insert the new question into the Response table
+           */    
+          var message = 'New Question: ' + question.get('questionText');
+          var newResponseObject = Parse.Object.extend('Response');
+          var newResponse = new newResponseObject();
+          newResponse.set('question', question);
+          newResponse.set('answer', '');
+          newResponse.set('reflection', task.get('reflection'));
+          newResponse.save();
+          /**
+           * Push the message
+           */
+          Parse.Push.send(
+            {
+                channels: [task.get('broadcastChannel')],
+                data: { alert: message, sound: 'default' }
+            },
+            {
+              success: function() {
+                status.success('Parse.Push.send completed successfully.');
+              },
+              error: function() {
+                status.error('Uh oh, something went wrong with Parse.Push.send.');
+              }
+            }
+          );
+        },
+        error: function() {
+          console.log('Unable to find a question.');
+        }
+      });
+    });
+    /**
+     * Mark Task complete
+     */
+    task.set('completed', true);
+    return task.save();
+  }).then(function() {
+    status.success('RuminateTask completed successfully.');
+  }, function(error) {
+    status.error('Uh oh, something went wrong.');
   });
-  // var query = new Parse.Query('RuminateTask');
-  // query.each(function(task) {
-  //   task.set('completed', true);
-  //   return task.save();
-  // }).then(function() {
-  //   status.success('RuminateTask completed successfully.');
-  // }, function(error) {
-  //   status.error('Uh oh, something went wrong.');
-  // });
 });

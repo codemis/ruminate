24b58d7a124939d90050536d9ae0d823e37c5d44
Codemis <johnathan@jpulos.com>
2016-05-24 22:05:25 -0700

    Removed Parse from the Controller.

--- app/scripts/controllers/history.controller.js
+++ app/scripts/controllers/history.controller.js
@@ -2,50 +2,83 @@
 
 var appControllers = angular.module('app.controllers');
 
-appControllers.controller('HistoryController', ['$scope', 'ParseReflection', 'ParsePassage', 'BibleAccessor'
-  , function($scope, ParseReflection, ParsePassage, BibleAccessor) {
+appControllers.controller('HistoryController', ['$scope', '$ionicPlatform', '$cordovaNetwork', 'onDeviceService', 'ConsumerService', 'RuminationService', function($scope, $ionicPlatform, $cordovaNetwork, onDeviceService, ConsumerService, RuminationService) {
+  /**
+   * Is the API accessible
+   *
+   * @type {Boolean}
+   * @access public
+   */
+  $scope.apiAccessible = true;
+  /**
+   * The current Consumer
+   *
+   * @type {Object}
+   * @access public
+   */
+  $scope.consumer = null;
+  /**
+   * A group of Ruminations
+   *
+   * @type {Array}
+   * @access public
+   */
+  $scope.ruminations = null;
 
-  //Use parse to fetch the history
 
-  //After that is done....place it in the $scope variable....
+  $ionicPlatform.ready(function() {
+    /**
+     * Handle online/offline access
+     */
+    if(onDeviceService.check()) {
+      $scope.apiAccessible = $cordovaNetwork.isOnline();
+    }
+    /**
+     * Watch for events that are broadcasted
+     */
+    $scope.$on('$cordovaNetwork:online', function() {
+      $scope.apiAccessible = true;
+      setup();
+    });
 
-  $scope.historyView = [];
+    $scope.$on('$cordovaNetwork:offline', function() {
+      $scope.apiAccessible = false;
+      teardown();
+    });
 
-  //var date = new Date();
+    $scope.$on('$ionicView.enter', function() {
+      setup();
+    });
 
-  //dateString = (date.getMonth() + 1).toString() + "-" + (date.getDate()).toString() + "-" + (date.getYear()).toString();
+    $scope.$on('$ionicView.leave', function() {
+      teardown();
+    });
 
-  var dateString = '10-03-2015';
+    $scope.$on('$destroy', function() {
+      teardown();
+    });
 
-  var numberOfQueriesWithReflection = 0;
-
-  var parseResult = [];
-
-  var dateOfToday = new Date(new Date().toDateString());
-  var dateOfTomorrow = new Date(new Date().toDateString());
-  dateOfTomorrow.setDate(dateOfTomorrow.getDate() + 1);
-
-  ParseReflection.query(dateOfToday, dateOfTomorrow, function(results) {
-    if(typeof results === 'undefined' || results.length === 0) { return; }
-    for (var i = 0; i < results.length; i++) {
-      var reflection = results[i];
-      ParsePassage.queryWithReflection(reflection, function(passages) {
-        if(typeof passages === 'undefined' || passages.length === 0) { return; }
-        for (var i = 0; i < passages.length; i++) {
-          var passage = passages[i];
-          var tmp = {
-            book: BibleAccessor.bookNames[passage.get('book')],
-            chapter: passage.get('chapter'),
-            start: passage.get('firstVerse'),
-            end: passage.get('lastVerse'),
-            verse: passage.get('snippet'),
-            date: reflection.get('createdAt').toDateString(),
-            objId: reflection.id
-          };
-          $scope.historyView.push(tmp)
-        };
-        $scope.$apply();
-      })
-    };
   });
+
+  /**
+   * Setup the controller
+   *
+   * @access private
+   */
+  function setup() {
+    ConsumerService.getCurrent().then(function(consumer) {
+      $scope.consumer = consumer;
+      RuminationService.all(consumer.apiKey, 'createdAt|desc').then(function(ruminations) {
+        $scope.ruminations = ruminations;
+      });
+    });
+  }
+
+  /**
+   * Teardown the controller
+   *
+   * @access private
+   */
+  function teardown() {
+  }
 }]);

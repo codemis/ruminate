d687f30e3b6802af3e5820c518e1e9394971c54c
Codemis <johnathan@jpulos.com>
2016-06-07 21:17:09 -0700

    Removed Parse and renamed app to ruminate.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,14 +1,8 @@
 'use strict';
-// Ionic Starter App
 
-// angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
-// the 2nd parameter is an array of 'requires'
-// 'starter.services' is found in services.js
-// 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'ngCordova', 'app.controllers', 'app.models', 'app.libraries', 'parse.services', 'dbp.services', 'ionic.utilities', 'monospaced.elastic', 'config', 'api-tokens.config'])
+angular.module('ruminate', ['ionic', 'ngCordova', 'app.controllers', 'app.models', 'app.libraries', 'dbp.services', 'ionic.utilities', 'monospaced.elastic', 'config', 'api-tokens.config'])
 
-.run(['$ionicPlatform', 'ParseService', 'PARSE_ENV', 'BibleAccessor', function($ionicPlatform, ParseService, PARSE_ENV, BibleAccessor) {
+.run(['$ionicPlatform', 'BibleAccessor', function($ionicPlatform, BibleAccessor) {
   $ionicPlatform.ready(function() {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs)
@@ -21,7 +15,6 @@ angular.module('starter', ['ionic', 'ngCordova', 'app.controllers', 'app.models'
       // org.apache.cordova.statusbar required
       StatusBar.styleLightContent();
     }
-    ParseService.initialize(PARSE_ENV.appId, PARSE_ENV.clientId, PARSE_ENV.jSKey);
     BibleAccessor.updateBookMaps();
   });
 }])
